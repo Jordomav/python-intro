9b25f614b7df10bbf3969f2dde92e8ca44ce222a
Jordomav <jordomav@gmail.com>
2018-02-17 23:27:51 +0000

    init

---
+++ main.py
@@ -0,0 +1,35 @@
+# print 'Hello World'
+# name = raw_input('Your name: ')
+# print name
+# if the robot runs into an object have the robot stop and back up
+# if hitsObject():
+    # goBack()
+#if 1 == 1:
+#     print 'Yes'
+# def myFirstFunction():
+#     print 'Function ran'
+# myFirstFunction()
+# def functionWithInput(name, age):
+#     print name
+#     print age
+# functionWithInput('Jordan', 21)
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# colors = ['red', 'blue', 'green']
+# print numbers[0]
+# for number in numbers:
+#     if number % 2 == 0:
+#         print number
+# for color in colors:
+#     if color == 'red':
+#         print 'You printed red'
+#     elif color == 'blue':
+#         print 'You printed blue'
+#     elif color == 'green':
+#         print 'You printed green'
+#     else:
+#         print 'Color not found'
+
+noun1 = raw_input('Please enter a noun:')
+verb1 = raw_input('Please enter a verb:')
+noun2 = raw_input('Please enter another noun:')
+verb2 = raw_input('Please enter another verb:')
